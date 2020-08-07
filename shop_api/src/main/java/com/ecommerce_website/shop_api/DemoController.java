a961681c675c3078d7d893f5ef06d09d8e8cc560
Ankit Kumar <ankit.kumar.btech21@gmail.com>
2020-08-07 19:01:11 +0530

    TODO :- sayHello endpoint implemented.

--- shop_api/src/main/java/com/ecommerce_website/shop_api/DemoController.java
+++ shop_api/src/main/java/com/ecommerce_website/shop_api/DemoController.java
@@ -1,4 +1,12 @@
 package com.ecommerce_website.shop_api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class DemoController {
+
+    @RequestMapping(value="/sayHello")
+    public String sayHello(){
+        return "Hello Sir/Madam, have a nice day";
+    }
 }
