16a07190511f8883e38204b477d7994cf6d6eadf
Ankit Kumar <ankit.kumar.btech21@gmail.com>
2020-08-07 21:20:18 +0530

    TODO: 	1. product entity added inside catalog. 	2. constants file added.

---
+++ shop_api/src/main/java/com/ecommerce_website/shop_api/Constants.java
@@ -0,0 +1,5 @@
+package com.ecommerce_website.shop_api;
+
+public class Constants {
+    public static String productUrl= "/products";
+}
---
+++ shop_api/src/main/java/com/ecommerce_website/shop_api/catalog/Product.java
@@ -0,0 +1,61 @@
+package com.ecommerce_website.shop_api.catalog;
+
+import com.ecommerce_website.shop_api.Constants;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @JsonIgnore
+        private Integer productId;
+        private String productName;
+        private String productDescription;
+
+        public Product() {
+            super();
+        }
+
+        public Product(Integer productId, String productName, String productDescription) {
+            super();
+            this.productId = productId;
+            this.productName = productName;
+            this.productDescription = productDescription;
+        }
+
+        @JsonProperty("productId")
+        public Integer getProductId() {
+            return productId;
+        }
+
+        @JsonIgnore
+        public void setProductId(Integer productId) {
+            this.productId = productId;
+        }
+
+        public String getProductName() {
+            return productName;
+        }
+
+        public void setProductName(String productName) {
+            this.productName = productName;
+        }
+
+        public String getProductDescription() {
+            return productDescription;
+        }
+
+        public void setProductDescription(String productDescription) {
+            this.productDescription = productDescription;
+        }
+
+        @JsonProperty("href")
+        public String getHREF() {
+            return Constants.productUrl + this.productId;
+        }
+}
