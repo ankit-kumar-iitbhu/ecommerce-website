a901ddca8b66c36459a7e3695ecd391fbb40855c
Ankit Kumar <ankit.kumar.btech21@gmail.com>
2020-08-08 22:50:51 +0530

    Action Taken : Required annotation added to Application class

--- shop_api/src/main/java/com/ecommerce_website/shop_api/Application.java
+++ shop_api/src/main/java/com/ecommerce_website/shop_api/Application.java
@@ -1,9 +1,17 @@
 package com.ecommerce_website.shop_api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.ecommerce_website.shop_api")
+@EnableAutoConfiguration
+@ComponentScan("com.ecommerce_website.shop_api")
+@Configuration
 public class Application {
 
 	public static void main(String[] args) {
